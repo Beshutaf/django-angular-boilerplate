60543468ac78bff0cb23a2148666e130b00c5d49
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-10-11 22:38:35 +0300

    Add base.html back

--- shift_report/urls.py
+++ shift_report/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^shifts$', views.index, name='index'),
     url(r'^shifts/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})$', views.detail),
     url(r'^members$', views.members),
     url(r'^members/delete$', views.delete),
