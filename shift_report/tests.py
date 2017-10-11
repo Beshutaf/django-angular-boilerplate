b83220a3ced3ac951bcd45174cee07ce8463bc03
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-10-11 22:03:42 +0300

    Add test

--- shift_report/tests.py
+++ shift_report/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Shift, MemberShift, Member, Role, User
+
+
+MEMBERS = [
+    ("יונתן", 'אחמ"ש'),
+    ("גלי", 'תורן'),
+    ("מיכל", 'תורן'),
+]
+
+
+class ShiftModelTests(TestCase):
+    def test_add_members(self):
+        s, is_new = Shift.objects.get_or_create(date=timezone.now())
+        for name, role in MEMBERS:
+            MemberShift.objects.create(member=Member.objects.create(user=User.objects.get_or_create(username=name)[0]),
+                                       shift=s, role=Role.objects.get_or_create(name=role)[0], shift_number=1)
+        self.assertEqual(len(s.members.all()), len(MEMBERS))
