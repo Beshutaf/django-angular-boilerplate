56b78d6c5cf9462d88f4a13872b538fc2cd1534f
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-10-11 22:03:24 +0300

    Refactor and remove unnecessary files

--- shift_report/views.py
+++ shift_report/views.py
@@ -4,19 +4,18 @@ from datetime import datetime
 from io import TextIOWrapper
 
 from django.contrib.auth.models import User
-from django.core.serializers.json import DjangoJSONEncoder
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 from shift_report.forms import MemberForm
-from shift_report.models import Member, Shift, MemberShift, Role
+from shift_report.models import Member, Shift
 
 
 def index(request):
     return detail(request, *datetime.now().date().isoformat().split("-"))
 
 
-def detail(request, year, month, day):
+def detail(request, *year_month_day):
     """
     {
      date: ...,
@@ -72,22 +71,13 @@ def detail(request, year, month, day):
      ],
     }
     """
-    date_str = "-".join((year, month, day))
-    s, is_new = Shift.objects.get_or_create(date=date_str)
-    # if request.method == "POST":
-    #     for key, value in request.POST.items():
-    #         print(key + ": " + str(value))  # TODO set field in shift
-    # s = Shift.objects.get_or_create(date=datetime.strptime(date_str, '%Y-%m-%d'))
-    MemberShift.objects.create(member=Member(user=User.objects.get_or_create(username='יונתן')[0]),
-                               shift=s, role=Role.objects.get_or_create(name='אחמ"ש')[0],
-                               shift_number=1)
-    MemberShift.objects.create(member=Member(user=User.objects.get_or_create(username='גלי')[0]),
-                               shift=s, role=Role.objects.get_or_create(name='תורן')[0],
-                               shift_number=1)
-    MemberShift.objects.create(member=Member(user=User.objects.get_or_create(username='מיכל')[0]),
-                               shift=s, role=Role.objects.get_or_create(name='תורן')[0],
-                               shift_number=1)
-    return JsonResponse(s.serialize())
+    s, _ = Shift.objects.get_or_create(date="-".join(year_month_day))
+    if request.method == "POST":
+        for key, value in request.POST.items():
+            setattr(s, key, value)
+        s.save()
+    return JsonResponse(s.serialize()) if request.GET.get("format") == "json" else \
+        render(request, "shifts/detail.html", {"shift": s})
 
 
 def members(request):
@@ -97,17 +87,14 @@ def members(request):
      ...
     ]
     """
-    for m in Member.objects.all():
-        m.delete()
     if request.method == "POST":
         if request.FILES:
             for d in csv.DictReader(TextIOWrapper(request.FILES["csv_file"])):
                 process_member(d)
         else:
             process_member(request.POST or None)
-    m = Member.objects.all
-    return DjangoJSONEncoder().encode(m) if request.GET.get("format") == "json" else \
-        render(request, "members/list.html", {"members": m, "form": MemberForm()})
+    return JsonResponse(m.username for m in Member.objects.all()) if request.GET.get("format") == "json" else \
+        render(request, "members/list.html", {"members": Member.objects.all(), "form": MemberForm()})
 
 
 def process_member(fields):
