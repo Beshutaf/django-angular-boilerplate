c54c6a080ca62aaaf2768f26df66eb4fab54af1c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-10-12 19:33:28 +0300

    Add update method to shift

--- shift_report/models.py
+++ shift_report/models.py
@@ -63,6 +63,61 @@ class Shift(models.Model):
                     disposed_products=[p.serialize() for p in self.disposed_products.all()],
                     )
 
+    def update(self, date=None, member_shifts=None, new_members=None, leaving_members=None, conclusions=None,
+               money_at_shift_start=None, money_at_shift_end=None, money_from_shares=None, money_from_cheques=None,
+               money_from_cash=None, envelope_number=None, almost_missing_products=None, disposed_products=None,
+               **kwargs):
+        if date is not None:
+            self.date = date
+        if member_shifts is not None:
+            members = []
+            for m in member_shifts:
+                member = self.get_or_create_member(m["member"])
+                member_shift, _ = MemberShift.objects.get_or_create(shift=self, member=member)
+                member_shift.role, _ = Role.objects.get_or_create(m["role"])
+                member_shift.shift_number = m["shift_number"]
+                member_shift.save()
+                members.append(member)
+            MemberShift.objects.filter(shift=self, member__not_in=members).delete()
+        if new_members is not None:
+            self.new_members = [self.get_or_create_member(m) for m in new_members]
+        if leaving_members is not None:
+            self.leaving_members = [self.get_or_create_member(m) for m in leaving_members]
+        if conclusions is not None:
+            conclusions_iter = iter(self.conclusions_set.all())
+            for d in conclusions:
+                try:
+                    c = next(conclusions_iter)
+                except StopIteration:
+                    c = Conclusions.objects.create(shift=self)
+                c.comment = d["comment"]
+                c.assigned_team = d["assigned_team"]
+                c.done = d["done"]
+            for c in conclusions_iter:
+                c.delete()
+        self.money_at_shift_start = money_at_shift_start
+        self.money_at_shift_end = money_at_shift_end
+        self.money_from_shares = money_from_shares
+        self.money_from_cheques = money_from_cheques
+        self.money_from_cash = money_from_cash
+        self.envelope_number = envelope_number
+        if almost_missing_products is not None:
+            self.almost_missing_products = []
+            for p in almost_missing_products:
+                product, _ = Product.objects.get_or_create(name=p)
+                self.almost_missing_products.append(p)
+        if disposed_products is not None:
+            self.disposed_products = []
+            for p in disposed_products:
+                product, _ = Product.objects.get_or_create(name=p)
+                self.disposed_products.append(p)
+        self.save()
+
+    def get_or_create_member(self, username):
+        user, _ = User.objects.get_or_create(username)
+        member, _ = Member.objects.get_or_create(user=user)
+        return member
+
 
 class MemberShift(models.Model):
     member = models.ForeignKey(Member)
