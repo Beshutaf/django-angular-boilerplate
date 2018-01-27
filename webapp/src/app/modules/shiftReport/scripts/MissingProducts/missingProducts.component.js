d6d377cc0d4fd4f575e0d7968a43888feebaf321
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-01-27 16:51:04 +0000

    fix missing product after add product, input filed gets empty

--- webapp/src/app/modules/shiftReport/scripts/MissingProducts/missingProducts.component.js
+++ webapp/src/app/modules/shiftReport/scripts/MissingProducts/missingProducts.component.js
@@ -26,6 +26,7 @@
             
             function addMissingProduct(productName) {
                 ctrl.productsList.push(productName);
+                ctrl.productName = ""
             }
             
             function removeProduct(productName){
