c20882bf4292b2a46ba520869a50221975c82b67
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-01-29 13:39:11 +0000

    bidn shift data to tasks component

--- webapp/src/app/modules/shiftReport/scripts/Tasks/tasks.component.js
+++ webapp/src/app/modules/shiftReport/scripts/Tasks/tasks.component.js
@@ -5,17 +5,19 @@
         .module('app.shiftReport')
         .component('tasks', {
             bindings: {
-                
+                tasks:"="
             },
             controller: tasksCtrl,
             templateUrl: '/app/modules/shiftReport/scripts/Tasks/tasks.template.html'
         });
 
+//TODO: add option to add more tasks
+
     function tasksCtrl() {
         var ctrl = this;
 
         ctrl.$onInit = function() {
-            ctrl.tasks = [];
+            
             var n = 5;
             for (var i = 0; i < n; i++)
                 ctrl.tasks.push({
