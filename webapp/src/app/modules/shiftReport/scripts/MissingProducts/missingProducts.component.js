a45c22bb08cef83f2df68ae731e2acebb92c9ddb
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-01-29 13:34:02 +0000

    bind report data to missing products component

--- webapp/src/app/modules/shiftReport/scripts/MissingProducts/missingProducts.component.js
+++ webapp/src/app/modules/shiftReport/scripts/MissingProducts/missingProducts.component.js
@@ -5,7 +5,7 @@
         .module('app.shiftReport')
         .component('missingProducts', {
             bindings: {
-                
+                productsList:"="
             },
             controller: missingProductsCtrl,
             templateUrl: '/app/modules/shiftReport/scripts/MissingProducts/missingProducts.template.html'
@@ -20,21 +20,18 @@
         /////////////////////////////////
         
         ctrl.title = "מוצרים שעומדים להיגמר או חסרים";
-        ctrl.productsList = [];
-        
-        
             
-            function addMissingProduct(productName) {
-                ctrl.productsList.push(productName);
-                ctrl.productName = ""
+        function addMissingProduct(productName) {
+            ctrl.productsList.push(productName);
+            ctrl.productName = ""
+        }
+        
+        function removeProduct(productName){
+            var index = ctrl.productsList.indexOf(productName)
+            if (index > -1){
+                ctrl.productsList.splice(index,1);
             }
-            
-            function removeProduct(productName){
-                var index = ctrl.productsList.indexOf(productName)
-                if (index > -1){
-                    ctrl.productsList.splice(index,1);
-                }
-            }
+        }
 
     }
 })();
